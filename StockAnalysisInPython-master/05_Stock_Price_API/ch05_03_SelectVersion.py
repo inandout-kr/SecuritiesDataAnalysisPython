31d3ee61023447c7b0772cb250248c6912e520c8
hellofinance <kiunghan@gmail.com>
2021-08-10 01:37:14 +0900

    Update ch05_03_SelectVersion.py

--- StockAnalysisInPython-master/05_Stock_Price_API/ch05_03_SelectVersion.py
+++ StockAnalysisInPython-master/05_Stock_Price_API/ch05_03_SelectVersion.py
@@ -1,11 +1,11 @@
 import pymysql
 
-connection = pymysql.connect(host='localhost', port=3306, db='INVESTAR', 
-    user='root', passwd='myPa$$word', autocommit=True)  
+connection = pymysql.connect(host='localhost', port=3306, db='dailystockpricedata', 
+    user='root', passwd='', autocommit=True)  # connect() 함수 사용해서 connection 객체 생성.
 
-cursor = connection.cursor()
-cursor.execute("SELECT VERSION();")
-result = cursor.fetchone()
+cursor = connection.cursor()  #cursor() 함수 사용해 cursor 객체 생성
+cursor.execute("SELECT VERSION();")  #cursor 객체의 execute() 함수를 사용해 SELECT문 실행
+result = cursor.fetchone()  # cursor 객체의 fetchone() 함수 사용해 바로 윗줄의 실행 결과를 튜플로 받음
 
 print ("MariaDB version : {}".format(result))
 
