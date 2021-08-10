99d5b84a3a1caf65466ac100ef586e4373826d1f
hellofinance <kiunghan@gmail.com>
2021-08-10 23:21:26 +0900

    DB

--- StockAnalysisInPython-master/05_Stock_Price_API/ch05_02_NaverDatabase_SEC.py
+++ StockAnalysisInPython-master/05_Stock_Price_API/ch05_02_NaverDatabase_SEC.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from Investar import Analyzer  # ①
 
 mk = Analyzer.MarketDB()  # ②
-df = mk.get_daily_price('005930', '2017-07-10', '2018-06-30')  # ③
+df = mk.get_daily_price('005930', '2017-07-10', '202-06-30')  # ③
 
 plt.figure(figsize=(9, 6))
 plt.subplot(2, 1, 1)
