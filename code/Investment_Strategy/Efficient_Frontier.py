31dde71daa3f28684635bcba19e04e2d04352d77
hellofinance <kiunghan@gmail.com>
2021-08-28 02:22:21 +0900

    Update Efficient_Frontier.py

--- code/Investment_Strategy/Efficient_Frontier.py
+++ code/Investment_Strategy/Efficient_Frontier.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
 from AnalyzerFolder import Analyzer
@@ -15,5 +15,24 @@ annual_ret = daily_ret.mean()
 daily_cov = daily_ret.cov()
 annual_cov = daily_cov * 252
 
-print(annual_ret)
-print(annual_cov)
+port_ret = []      # 종목 비중 다르게 해 포트 20,000개 생성하기 위한 단계
+port_risk = []     # 포트 수익률, 리스크, 비중 저장할 리스트 생성
+port_weights = []  #
+
+for _ in range(20000): # 몬테카를로 시뮬레이션 이용해 portfolio 20,000개 생성
+    weights = np.random.random(len(stock))  # stock 개수의 랜덤 숫자로 구성된 배열 생성
+    weights /= np.sum(weights)  # 비중의 합이 1이 되도록 조정
+
+    returns = np.dot(weights, annual_ret)  # np.dot 사용해 [종목별 비중 배열 * 종목별 연간 수익률 = 해당 portfolio 전체 returns] 구하기
+    risk = np.sqrt(np.dot(weights.T, np.dot(annual_cov, weights))) 
+
+    port_ret.append(returns) 
+    port_risk.append(risk) 
+    port_weights.append(weights) 
+
+portfolio = {'Returns': port_ret, 'Risk': port_risk} 
+for i, s in enumerate(stock): 
+    portfolio[s] = [weight[i] for weight in port_weights] 
+df = pd.DataFrame(portfolio) 
+df = df[['Returns', 'Risk'] + [s for s in stock]]
+
